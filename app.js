a89c023d2917d1ca45915670a5011c9bacf786da
TayyabTahir0607 <155762139+TayyabTahir0607@users.noreply.github.com>
2024-01-13 01:58:18 +0500

    add

---
+++ app.js
@@ -0,0 +1,41 @@
+let Api1 ="https://api.openweathermap.org/data/2.5/weather?q=";
+let Api2 ="&appid=ab533be059643ed243915301feb06b9c&units=metric";
+const inputCity= document.querySelector(".search-bar input");
+const btn = document.querySelector(".search-bar i");
+const updateCity=document.querySelector(".temp-country #city");
+const updateTemp=document.querySelector(".temp-country #temp");
+const updateHumid=document.querySelector(".display-box1 #humi");
+const updateWind=document.querySelector(".display-box2 #W-speed");
+const updateCloud=document.querySelector(".weather-img img");
+
+btn.addEventListener("click",cty=()=>{
+    let cityName=inputCity.value;
+    
+    
+    (fetchWeather=async()=>{
+        console.log("fetching data....")
+        let data=await fetch(`${Api1}${cityName}${Api2}`);
+        console.log("converting data into json object....")
+        data= await data.json();
+        console.log(data);
+        updateCity.innerText=cityName;
+        updateTemp.innerText= `${Math.round(data.main.temp)}°C`;
+        updateWind.innerText=`${data.wind.speed} km/h`;
+        updateHumid.innerText=`${data.main.humidity}`;
+        let cld=data.weather[0].main;
+        if(cld=="Clouds")
+        {
+            updateCloud.src="clouds.png";
+        }else if(cld=="Mist")
+        {
+            updateCloud.src="mist.png"
+        }else if(cld=="dizzle")
+        {
+            updateCloud.src="dizzle.png"
+        }
+        else{
+            updateCloud.src="clear.png"
+        }
+    })();
+
+});
